bc9079785ecb22981252f8405d85e97f2d9b0afe
Suryakx <sxc45540@ucmo.edu>
2024-05-20 23:06:50 -0500

    Add files via upload

---
+++ Code Outputs.py
@@ -0,0 +1,81 @@
+Python 3.12.3 (tags/v3.12.3:f6650f9, Apr  9 2024, 14:05:25) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============================================================ RESTART: C:/Users/surya/OneDrive/Desktop/Assignment 1/code 1.py ===========================================================
+Sorted list: [19, 19, 20, 22, 24, 24, 24, 25, 25, 26]
+Minimum age: 19
+Maximum age: 26
+List with min and max added: [19, 19, 20, 22, 24, 24, 24, 25, 25, 26, 19, 26]
+Median age: 24.0
+Average age: 22.75
+Range of ages: 7
+>>> 
+============================================================== RESTART: C:/Users/surya/OneDrive/Desktop/Assignment 1/Q2.py =============================================================
+Dog Dictionary: {'name': 'Buddy', 'color': 'Brown', 'breed': 'Labrador', 'legs': 4, 'age': 3}
+
+Student Dictionary: {'first_name': 'John', 'last_name': 'Doe', 'gender': 'Male', 'age': 25, 'marital_status': 'Single', 'skills': ['Python', 'Java', 'SQL'], 'country': 'USA', 'city': 'New York', 'address': '123 Main St'}
+
+Length of Student Dictionary: 9
+
+Student Skills: ['Python', 'Java', 'SQL']
+Data Type of Skills: <class 'list'>
+
+Updated Student Skills: ['Python', 'Java', 'SQL', 'React', 'Angular']
+
+Student Dictionary Keys: ['first_name', 'last_name', 'gender', 'age', 'marital_status', 'skills', 'country', 'city', 'address']
+
+Student Dictionary Values: ['John', 'Doe', 'Male', 25, 'Single', ['Python', 'Java', 'SQL', 'React', 'Angular'], 'USA', 'New York', '123 Main St']
+>>> 
+============================================================== RESTART: C:/Users/surya/OneDrive/Desktop/Assignment 1/Q3.py =============================================================
+Siblings Tuple: ('Rachel', 'Emily', 'Sophia', 'Michael', 'David', 'Alex')
+Number of Siblings: 6
+Family Members Tuple: ('Rachel', 'Emily', 'Sophia', 'Michael', 'David', 'Alex', 'John', 'Jane')
+>>> 
+============================================================== RESTART: C:/Users/surya/OneDrive/Desktop/Assignment 1/Q4.py =============================================================
+Length of it_companies: 7
+Updated it_companies: {'Twitter', 'Microsoft', 'Google', 'Apple', 'Facebook', 'Oracle', 'Amazon', 'IBM'}
+it_companies after adding multiple companies: {'Oracle', 'IBM', 'Facebook', 'Amazon', 'Twitter', 'Samsung', 'Intel', 'Microsoft', 'Google', 'Apple', 'Nvidia'}
+it_companies after removing IBM: {'Oracle', 'Facebook', 'Amazon', 'Twitter', 'Samsung', 'Intel', 'Microsoft', 'Google', 'Apple', 'Nvidia'}
+Difference between remove() and discard():
+remove() raises an error if the element is not found.
+discard() does not raise an error if the element is not found.
+A union B: {19, 20, 22, 24, 25, 26, 27, 28}
+A intersection B: {19, 20, 22, 24, 25, 26}
+Is A a subset of B? True
+Are A and B disjoint sets? False
+A union B: {19, 20, 22, 24, 25, 26, 27, 28}
+B union A: {19, 20, 22, 24, 25, 26, 27, 28}
+Symmetric difference between A and B: {27, 28}
+Ages List: [22, 19, 24, 25, 26, 24, 25, 24]
+Ages Set: {19, 22, 24, 25, 26}
+Length of Ages List: 8
+Length of Ages Set: 5
+
+============================================================== RESTART: C:/Users/surya/OneDrive/Desktop/Assignment 1/Q5.py =============================================================
+Area of the circle with radius 30 meters: 2827.43 square meters
+Circumference of the circle with radius 30 meters: 188.50 meters
+Enter the radius of the circle (in meters): 
+============================================================== RESTART: C:/Users/surya/OneDrive/Desktop/Assignment 1/Q6.py =============================================================
+Words in the sentence: ['I', 'am', 'a', 'teacher', 'and', 'I', 'love', 'to', 'inspire', 'and', 'teach', 'people']
+Unique words: {'to', 'people', 'inspire', 'teacher', 'teach', 'I', 'am', 'and', 'a', 'love'}
+Number of unique words: 10
+
+============================================================== RESTART: C:/Users/surya/OneDrive/Desktop/Assignment 1/Q7.py =============================================================
+Name	Age	Country	City
+Asabeneh	250	Finland	Helsinki
+
+============================================================== RESTART: C:/Users/surya/OneDrive/Desktop/Assignment 1/Q8.py =============================================================
+The area of a circle with radius 10 is 314 meters square.
+
+============================================================== RESTART: C:/Users/surya/OneDrive/Desktop/Assignment 1/Q9.py =============================================================
+Enter the number of students: 5
+Enter the weights in pounds (lbs):
+Weight of student 1: 195
+Weight of student 2: 200
+Weight of student 3: 190
+Weight of student 4: 180
+Weight of student 5: 170
+
+Original weights in pounds (lbs): [195.0, 200.0, 190.0, 180.0, 170.0]
+Converted weights in kilograms (kg): [88.45044, 90.7184, 86.18248, 81.64656, 77.11064]
+\
